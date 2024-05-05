4cae4e30a896236a02574acfadfe90b9bcf55e60
alexcruz097 <alexc017@yahoo.com>
2024-05-05 00:01:00 -0700

    able to draw from deck

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [deckID, setDeckID] = useState("");
+  const [cards, setCards] = useState([]);
+  const [cardRemaining, setCardRemaining] = useState(null);
+  // get the id of the deck
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data: response } = await axios.get(
+          "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+        );
+        setDeckID(response.deck_id);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const drawDeck = async () => {
+    try {
+      const data = await axios.get(
+        `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1
+    `
+      );
+      // push  image to array
+      setCards([...cards, data.data.cards[0].image]);
+
+      // set card remaining
+      setCardRemaining(data.data.remaining)
+      console.log(cardRemaining)
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
-      <h1>Hello World</h1>
+      <button onClick={drawDeck}>Game a Card</button>
+      <div className="cards-container">
+        {
+          cards.map((img)=>{
+            return <img src={img}/>
+          })
+        }
+      </div>
     </>
   );
 }
